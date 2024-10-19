d2ac29c7a6a6f815e44859c39484345c0fd28dae
Devy <izabela.marcinkowska@outlook.com>
2024-10-19 20:32:12 +0200

    first commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Izabela Marcinkowska on 2024-10-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
